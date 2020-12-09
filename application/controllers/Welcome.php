13c2b6322e063e58844b97dcb060023f05884492
RON <h4ckw1z@gmail.com>
2020-12-09 22:34:22 +0800

    update system files (database.php and database function inside controller.php)

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,13 +4,6 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 class Welcome extends Controller{
 
 	public function index() {
-		//$this->load->view('welcome_page');
-		//$this->load->library('database');
-		$this->load->database();
-		var_dump($this->db->table('comments')->getAll());
-	}
-
-	public function sa() {
-		var_dump($this->db->table('comments')->getAll());
+		$this->load->view('welcome_page');
 	}
 }
