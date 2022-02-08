41584dbbfe9ed515edab349d18a7a36c242bc1d7
RON <h4ckw1z@gmail.com>
2022-02-08 13:26:08 +0800

    Update version 3

--- app/controllers/Welcome.php
+++ app/controllers/Welcome.php
@@ -5,11 +5,5 @@ class Welcome extends Controller {
 	public function index() {
 		$this->call->view('welcome_page');
 	}
-
-	public function t() {
-		$this->call->database();
-
-		var_dump($this->db->table('member')->where('id', 9)->update(array('username'=>'ronron')));
-	}
 }
 ?>
