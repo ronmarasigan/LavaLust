320d55d2e815f662372ae92b3ae59b44919f3fcb
Wingman-1034 <kingumali63@gmail.com>
2025-07-09 21:46:05 +0800

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['ENVIRONMENT']             = 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= '';
+$config['base_url'] 				= 'http://localhost/LavaLust/';
 
 /*
 |--------------------------------------------------------------------------
